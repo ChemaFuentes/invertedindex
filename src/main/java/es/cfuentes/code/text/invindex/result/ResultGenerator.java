e6b46390e119f24e948950ee3e10ca84c207d740
Chema Fuentes <ChemaFuentes@users.noreply.github.com>
2019-06-25 18:05:40 +0200

    Updating result generation

--- src/main/java/es/cfuentes/code/text/invindex/result/ResultGenerator.java
+++ src/main/java/es/cfuentes/code/text/invindex/result/ResultGenerator.java
@@ -1,13 +1,22 @@
 package es.cfuentes.code.text.invindex.result;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.MapPropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.comparator.Comparators;
 
 import es.cfuentes.code.text.invindex.beans.ScoredDocument;
 import es.cfuentes.code.text.invindex.index.InvertedIndex;
@@ -18,6 +27,9 @@ public class ResultGenerator {
 	@Value("${monitor.terms}")
 	private String[] terms;
 	
+	@Value("${num.docs}")
+	private int limit;
+	
 	@Autowired
 	private InvertedIndex ie;
 
@@ -25,18 +37,24 @@ public class ResultGenerator {
 	public void printResults() {
 		System.out.println("Imprimo resultados");
 		
-		Set<ScoredDocument> combinedSet = new HashSet<ScoredDocument>();
+		//Set<ScoredDocument> combinedSet = new HashSet<ScoredDocument>();
 		
-		for(String term : terms)
-			combinedSet.addAll(ie.getTerm(term));
-		
-		combinedSet.stream().collect(Collectors.groupingByConcurrent(
-				ScoredDocument::getName,
-				Collectors.summingDouble(ScoredDocument::getScore));
-		// make a stream
+		//for(String term : terms)
+			//combinedSet.addAll(ie.getTerm(term));
+
+		// make a stream		
 		// group by document
-		// average
-		// sort
-		// filter
+		ConcurrentMap<String, Double> comMap = Arrays.stream(terms)
+			.map(term -> ie.getTerm(term))
+			.flatMap(Set::stream)			
+			.collect(Collectors.groupingByConcurrent(ScoredDocument::getName,
+				Collectors.summingDouble(ScoredDocument::getScore)));
+			
+		
+		comMap.entrySet().stream()
+			.map(entry -> new ScoredDocument(entry.getKey(), entry.getValue()/terms.length))
+			.sorted(Comparator.comparing(ScoredDocument::getScore).reversed())
+			.limit(limit)
+			.forEach(sd -> System.out.println(sd.getName() + " " + sd.getScore()));
 	}
 }
